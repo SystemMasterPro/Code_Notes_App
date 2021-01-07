26dc27b1d3db8d69c8222c6e506fc020b80d6245
Richard <richardjimenez.9641@gmail.com>
2021-01-07 14:42:24 -0500

    Conectando Atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/notes-db-app', {
+// mongoose.connect('mongodb://localhost:27017/notes-db-app', {
+mongoose.connect('mongodb+srv://richard:richarddev@cluster0.fkvje.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
