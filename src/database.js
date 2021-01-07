eedf922531a193f3f731037122f401448c0d740f
Richard <richardjimenez.9641@gmail.com>
2021-01-07 15:49:32 -0500

    Mejoras de Bootstrap

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
+// LOCAL
 // mongoose.connect('mongodb://localhost:27017/notes-db-app', {
+// PRODUCCION
 mongoose.connect('mongodb+srv://richard:richarddev@cluster0.fkvje.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
